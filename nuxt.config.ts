e1df6a56b40ce8831b91ed919616e065e3a6d815
ahyaemon <iamahyaemon@gmail.com>
2019-02-27 19:20:51 +0900

    build設定修正

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,4 +6,10 @@ export default {
     '@/assets/css/style.css',
   ],
   mode: 'spa',
+  build: {
+    publicPath: 'https://bancha-project.github.io/bancha-convertor2/',
+  },
+  router: {
+    base: '/bancha-convertor2/'
+  },
 }
